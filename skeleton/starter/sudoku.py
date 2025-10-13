d0d769cf703c634f701d410624e59073d89796b0
sunnysarkar <sarkarsunny29@gmail.com>
2025-10-12 17:38:31 -0700

    Add rest of backtracking alg

--- skeleton/starter/sudoku.py
+++ skeleton/starter/sudoku.py
@@ -77,6 +77,17 @@ class CSP(object):
         print(self.domain)
         print(self.connections)
 
+    def assignNode(self, key, value):
+        newCspInstance = CSP(self.board.copy(), self.domain.copy(), self.MRVHeap.copy())
+
+        newCspInstance.board[key] = value
+        newCspInstance.assignment[key] = value
+        newCspInstance.domain[key] = set(value)
+        newCspInstance.domainLen[key] = 1
+        forwardCheckResult = newCspInstance.forwardChecking(key)
+
+        return newCspInstance if forwardCheckResult else False
+
     def forwardChecking(self, key):
         connectionHeap = self.getConnectionsHeap(key)
 
@@ -94,70 +105,6 @@ class CSP(object):
                 pass
 
         return True
-
-    # def getConnections(self, inputCell):
-    #     connection = {}
-
-    #     inputRow = inputCell[0]
-    #     inputCol = inputCell[1]
-
-    #     # Get Row Connections
-    #     cols = set(range(1,10)).remove(inputCol)
-
-    #     row = {}
-    #     for col in cols:
-    #         connection[f"{inputRow}{col}"] = self.domain[f"{inputRow}{col}"]
-
-    #     # Get Col Connections
-    #     rows = set(chr(i) for i in range(65, 74)).remove(inputRow)
-
-    #     col = {}
-    #     for row in rows:
-    #         connection[f"{row}{inputCol}"] = self.domain[f"{row}{inputCol}"]
-
-    #     # Get Square Connections
-    #     cols = []
-    #     if int(inputCol) <= 3:
-    #         cols = ['1', '2', '3']
-    #     elif int(inputCol) >= 7:
-    #         cols = ['7', '8', '9']
-    #     else:
-    #         cols = ['4', '5', '6']
-
-    #     rows = []
-    #     if ord(inputRow) - 64 <= 3:
-    #         rows = ['A', 'B', 'C']
-    #     elif ord(inputRow) - 64 >= 7:
-    #         rows = ['G', 'H', 'I']
-    #     else:
-    #         rows = ['D', 'E', 'F']
-
-    #     square = {}
-    #     squarekeys = set()
-    #     for row in rows:
-    #         for col in cols:
-    #             cell = f"{row}{col}"
-
-    #             if col == inputCol or row == inputRow:
-    #                 """
-    #                 A1 | A2 | A3
-    #                 -------------
-    #                 B1 | B2 | B3
-    #                 -------------
-    #                 C1 | C2 | C3
-
-    #                 If Input Cell == B2
-    #                     -> Row B Added from getRows
-    #                     -> Col 2 Added from getCols
-
-    #                 Avoid Duplicates don't add
-    #                 """
-    #                 pass
-    #             else:
-    #                 connection[cell] = self.domain[cell]
-
-    #     return connection
-        
 
     def getConnectionsHeap(self, inputCell):
         connectionHeap = []
@@ -223,178 +170,39 @@ class CSP(object):
 
         return connectionHeap
 
-    def getConnections(self, inputCell):
-        connection = {}
-        connectionHeap = []
-        connectionKeys = set()
-
-        inputRow = inputCell[0]
-        inputCol = inputCell[1]
-
-        # Get Row Connections
-        cols = set(range(1,10)).remove(inputCol)
-
-        row = {}
-        for col in cols:
-            connection[f"{inputRow}{col}"] = self.domain[f"{inputRow}{col}"]
-            heapq.heappush(connectionHeap, (len(self.domain[f"{inputRow}{col}"]), self.boardIdx[f"{inputRow}{col}"], f"{inputRow}{col}"))
-            connectionKeys.add(f"{inputRow}{col}")
-
-
-        # Get Col Connections
-        rows = set(chr(i) for i in range(65, 74)).remove(inputRow)
-
-        col = {}
-        for row in rows:
-            connection[f"{row}{inputCol}"] = self.domain[f"{row}{inputCol}"]
-            heapq.heappush(connectionHeap, (len(self.domain[f"{row}{inputCol}"]), self.boardIdx[f"{row}{inputCol}"], f"{row}{inputCol}"))
-            connectionKeys.add(f"{row}{inputCol}")
-
-        # Get Square Connections
-        cols = []
-        if int(inputCol) <= 3:
-            cols = ['1', '2', '3']
-        elif int(inputCol) >= 7:
-            cols = ['7', '8', '9']
-        else:
-            cols = ['4', '5', '6']
-
-        rows = []
-        if ord(inputRow) - 64 <= 3:
-            rows = ['A', 'B', 'C']
-        elif ord(inputRow) - 64 >= 7:
-            rows = ['G', 'H', 'I']
-        else:
-            rows = ['D', 'E', 'F']
-
-        square = {}
-        squarekeys = set()
-        for row in rows:
-            for col in cols:
-                cell = f"{row}{col}"
-
-                if col == inputCol or row == inputRow:
-                    """
-                    A1 | A2 | A3
-                    -------------
-                    B1 | B2 | B3
-                    -------------
-                    C1 | C2 | C3
-
-                    If Input Cell == B2
-                        -> Row B Added from getRows
-                        -> Col 2 Added from getCols
-
-                    Avoid Duplicates don't add
-                    """
-                    pass
-                else:
-                    connection[cell] = self.domain[cell]
-                    heapq.heappush(connectionHeap, (len(self.domain[cell]), self.boardIdx[cell], cell))
-                    connectionKeys.add(cell)
-
-        return connection, connectionHeap, connectionKeys
-
-    # def getRow(self, inputCell):
-    #     inputRow = inputCell[0]
-    #     inputCol = inputCell[1]
-
-    #     cols = set(range(1,10)).remove(inputCol)
-
-    #     row = {}
-    #     rowHeap = []
-    #     for col in cols:
-    #         row[f"{inputRow}{col}"] = self.domain[f"{inputRow}{col}"]
-    #         heapq.heappush(rowHeap, (len(self.domain[f"{inputRow}{col}"]), self.boardIdx[f"{inputRow}{col}"]))
-    #         rowKeys.add(f"{row}{inputCol}")
-
-    #     return row, rowKeys
-
-    # def getCol(self, inputCell):
-    #     inputRow = inputCell[0]
-    #     inputCol = inputCell[1]
-
-    #     rows = set(chr(i) for i in range(65, 74)).remove(inputRow)
-
-    #     col = {}
-    #     colKeys = set()
-    #     for row in rows:
-    #         col[f"{row}{inputCol}"] = self.domain[f"{row}{inputCol}"]
-    #         colKeys.add(f"{row}{inputCol}")
-
-    #     return col, colKeys
-
-    # def getSquare(self, inputCell):
-    #     inputRow = inputCell[0]
-    #     inputCol = inputCell[1]
-
-    #     cols = []
-    #     if int(inputCol) <= 3:
-    #         cols = ['1', '2', '3']
-    #     elif int(inputCol) >= 7:
-    #         cols = ['7', '8', '9']
-    #     else:
-    #         cols = ['4', '5', '6']
-
-    #     rows = []
-    #     if ord(inputRow) - 64 <= 3:
-    #         rows = ['A', 'B', 'C']
-    #     elif ord(inputRow) - 64 >= 7:
-    #         rows = ['G', 'H', 'I']
-    #     else:
-    #         rows = ['D', 'E', 'F']
-
-    #     square = {}
-    #     squarekeys = set()
-    #     for row in rows:
-    #         for col in cols:
-    #             cell = f"{row}{col}"
-
-    #             if col == inputCol or row == inputRow:
-    #                 """
-    #                 A1 | A2 | A3
-    #                 -------------
-    #                 B1 | B2 | B3
-    #                 -------------
-    #                 C1 | C2 | C3
-
-    #                 If Input Cell == B2
-    #                     -> Row B Added from getRows
-    #                     -> Col 2 Added from getCols
-
-    #                 Avoid Duplicates don't add
-    #                 """
-    #                 pass
-    #             else:
-    #                 square[cell] = self.domain[cell]
-    #                 squarekeys.add(cell)
-    #     return square, squarekeys
-
-
 def backtracking(board):
     """Takes a board and returns solved board."""
     # TODO: implement this
-    csp_instance = CSP(board)
-    print(board)
+    csp = CSP(board)
 
-    backtracking_recursive({}, csp)
-    solved_board = board
-    return solved_board
+    return backtracking_recursive(csp)
 
-def backtracking_recursive(assignment, board):
+def backtracking_recursive(csp):
+    if checkComplete(csp.assignment):
+        return csp.board
+    
+    else:
+        while csp.MRVHeap:
+            domainLen, _, key = heapq.heappop(csp.MRVHeap)
 
+            if key not in csp.assignment and csp.domainLen[key] == domainLen:
+                break
+
+        for value in csp.domain[key]:
+            newCsp = csp.assignNode(key, value)
+
+            if newCsp is not False:
+                result = backtracking_recursive(newCsp)
+                if result is not False:
+                    return result
+            else:
+                return False
     return False
 
 def checkComplete(assignment):
     if len(assignment) == 81:
         return True
     return False
-
-def minimumRemainingValueHeuristic(csp):
-    return idx
-
-
-
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
