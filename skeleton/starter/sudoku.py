07e87ef34329601d80f9ef9f4009ff62e00631fe
sunnysarkar <sarkarsunny29@gmail.com>
2025-10-22 22:05:51 -0700

    Passed all testsgit add . Hooray :)

--- skeleton/starter/sudoku.py
+++ skeleton/starter/sudoku.py
@@ -41,172 +41,7 @@ def board_to_string(board):
             ordered_vals.append(str(board[r + c]))
     return ''.join(ordered_vals)
 
-
-# class CSP(object):
-#     def __init__(self, board, _domain=None, _domainLen=None, _assignment=None, _unassignedDomainLen=None):
-#         self.board = board
-#         self.boardIdx = _boardIdx
-#         self.domain = {}
-#         self.domainLen = {}
-#         self.unassignedDomainLen = {}
-#         self.MRVHeap = []
-#         self.assignment = {}
-
-#         if _domain is None:
-#             initialized = set()
-
-#             counter = 1
-#             for row in ROW:
-#                 for col in COL:
-#                     key = f"{row}{col}"
-#                     value = self.board[key]
-#                     _domain, _domainLen = (set([value]), 1) if value != 0 else (set(range(1,10)), 9)
-#                     if _domainLen == 1:
-#                         self.assignment[key] = value
-#                     else:
-#                         self.unassignedDomainLen[key] = _domainLen
-
-#                     self.domain[key] = _domain
-#                     self.domainLen[key] = _domainLen
-
-#                     counter += 1
-
-#             for key in self.assignment.keys():
-#                 self.forwardChecking(key)
-#         else:
-#             print("Using provided domain, domainLen, MRVHeap, assignment")
-#             # import pdb; pdb.set_trace()
-#             if None in [_domain, _domainLen, _assignment, _unassignedDomainLen]:
-#                 raise ValueError("Either provide all or none of _domain, _domainLen, _MRVHeap, _assignment")
-#             self.domain              = _domain
-#             self.domainLen           = _domainLen
-#             self.assignment          = _assignment
-#             self.unassignedDomainLen = _unassignedDomainLen
-
-
-#     def assignNode(self, key, value):
-#         # if key == 'A3' and value == 8:
-#         #     import pdb; pdb.set_trace()
-#         newBoard, newDomain, newDomainLen, newAssignment, newUnassignedDomainLen = (self.board.copy(), self.domain.copy(), self.domainLen.copy(), self.assignment.copy(), self.unassignedDomainLen.copy())
-
-#         newBoard[key] = value
-#         newAssignment[key] = value
-#         newDomain[key] = set([value])
-#         newDomainLen[key] = 1
-#         newUnassignedDomainLen.pop(key, None)
-
-#         # import pdb; pdb.set_trace()
-#         newCspInstance = CSP(newBoard, newDomain, newDomainLen, newAssignment, newUnassignedDomainLen)
-        
-
-#         forwardCheckResult = newCspInstance.forwardChecking(key)
-
-#         return newCspInstance if forwardCheckResult else False
-
-#     def forwardChecking(self, key):
-#         connectionHeap = self.getConnectionsHeap(key)
-
-
-
-#         while connectionHeap:
-#             _, _, connectionKey = heapq.heappop(connectionHeap)
-            
-#             if self.board[key] in self.domain[connectionKey]:
-#                 print(f"Forward Checking {key} -> {connectionKey} removing {self.board[key]}")
-#                 print("Before", connectionKey, self.domain[connectionKey], self.domainLen[connectionKey])
-#                 if connectionKey in self.unassignedDomainLen:
-#                     self.unassignedDomainLen[connectionKey] -= 1
-#                 self.domainLen[connectionKey] -= 1
-#                 if self.domainLen[connectionKey] == 0:
-#                     return False
-#                 self.domain[connectionKey].remove(self.board[key])
-#                 heapq.heappush(self.MRVHeap, (self.domainLen[connectionKey], self.boardIdx[connectionKey], connectionKey))
-
-#             else:
-#                 pass
-
-#         return True
-
-#     def getConnectionsHeap(self, inputCell):
-#         connectionHeap = []
-
-#         inputRow = inputCell[0]
-#         inputCol = inputCell[1]
-
-#         # Get Row Connections
-#         cols = set(range(1,10))
-#         cols.remove(int(inputCol))
-
-#         for col in cols:
-#             heapq.heappush(connectionHeap, (len(self.domain[f"{inputRow}{col}"]), self.boardIdx[f"{inputRow}{col}"], f"{inputRow}{col}"))
-
-
-#         # Get Col Connections
-#         rows = set(chr(i) for i in range(65, 74))
-#         rows.remove(inputRow)
-
-#         for row in rows:
-#             heapq.heappush(connectionHeap, (len(self.domain[f"{row}{inputCol}"]), self.boardIdx[f"{row}{inputCol}"], f"{row}{inputCol}"))
-
-#         # Get Square Connections
-#         cols = []
-#         if int(inputCol) <= 3:
-#             cols = ['1', '2', '3']
-#         elif int(inputCol) >= 7:
-#             cols = ['7', '8', '9']
-#         else:
-#             cols = ['4', '5', '6']
-
-#         rows = []
-#         if ord(inputRow) - 64 <= 3:
-#             rows = ['A', 'B', 'C']
-#         elif ord(inputRow) - 64 >= 7:
-#             rows = ['G', 'H', 'I']
-#         else:
-#             rows = ['D', 'E', 'F']
-
-#         for row in rows:
-#             for col in cols:
-#                 cell = f"{row}{col}"
-
-#                 if col == inputCol or row == inputRow:
-#                     """
-#                     A1 | A2 | A3
-#                     -------------
-#                     B1 | B2 | B3
-#                     -------------
-#                     C1 | C2 | C3
-
-#                     If Input Cell == B2
-#                         -> Row B Added from getRows
-#                         -> Col 2 Added from getCols
-
-#                     Avoid Duplicates don't add
-#                     """
-#                     pass
-#                 else:
-#                     heapq.heappush(connectionHeap, (len(self.domain[cell]), self.boardIdx[cell], cell))
-
-#         return connectionHeap
-    
-
-
-
-    
-
-# def backtracking2(board):
-#     """Takes a board and returns solved board."""
-#     # TODO: implement this
-
-#     csp = CSP(board)
-
-
-#     retval = backtracking_recursive(csp)
-
-#     return retval
-
-
-class CSP2(object):
+class CSP(object):
     def __init__(self, board, _domain=None, _domainLen=None):
         self.board = board
         self.domain = {}
@@ -309,11 +144,8 @@ class CSP2(object):
 
         return connectionHeap
 
-
-
 def backtracking(board):
-    csp = CSP2(board)
-    retval = backtrack(csp)
+    return backtrack({}, CSP(board))
 
 def backtrack(assignment, csp):
     if checkComplete(assignment):
@@ -342,72 +174,6 @@ def backtrack(assignment, csp):
         csp.board[key] = 0
         return False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def backtracking2(board):
-#     """Takes a board and returns solved board."""
-#     # TODO: implement this
-
-#     csp = CSP(board)
-
-
-#     retval = backtracking_recursive(csp)
-
-#     return retval
-
-# def backtracking_recursive(csp):
-#     # import pdb; pdb.set_trace()
-#     global cycleCounter
-#     print(f"\n\n\nCurrent Board {cycleCounter}: ")
-
-#     cycleCounter += 1
-#     # print(csp.board)
-#     if checkComplete(csp.assignment):
-#         return csp.board
-    
-#     else:
-#         # while csp.unassignedDomainLen:
-#         print("Unassigned Domain Len", csp.unassignedDomainLen)
-
-#         minLen = key = None
-#         for _key, _domainLen in csp.domainLen.items():
-#             if _key not in csp.assignment:
-#                 if minLen is None or _domainLen < minLen:
-#                     minLen = _domainLen
-#                     key = _key
-                
-#         for value in csp.domain[key]:
-#             print("===================================")
-#             print(f"Selected Key: {key}")
-#             print("csp.domain of key", key, csp.domain[key])
-#             print(f"Selected value: {value}")
-#             print("Old", csp.domain)
-#             newCsp = csp.assignNode(key, value)
-#             print("New", newCsp.domain if newCsp is not False else "No new CSP")
-
-#             if newCsp is False:
-#                 print("Backtracking from ", key, value)
-#                 return False
-#             else:
-#                 print("Calling Recursive")
-#                 result = backtracking_recursive(newCsp)
-#                 if result is not False:
-#                     return result
-#             # else:
-#             #     print("Skipping ", key, csp.domainLen[key], domainLen)
-#     return False
-
 def checkComplete(assignment):
     if len(assignment) == 81:
         return True
@@ -427,6 +193,7 @@ if __name__ == '__main__':
 
         solved_board = backtracking(board)
         # Write board to file
+        print_board(solved_board)
         print(board_to_string(solved_board))
         out_filename = 'output.txt'
         outfile = open(out_filename, "w")
