e385dae6b811592875b5581fa09f2e78bfbc7ebc
sunnysarkar <sarkarsunny29@gmail.com>
2025-10-22 21:38:08 -0700

    Align code to look more like the notes

--- skeleton/starter/sudoku.py
+++ skeleton/starter/sudoku.py
@@ -188,8 +188,154 @@ class CSP(object):
                     heapq.heappush(connectionHeap, (len(self.domain[cell]), self.boardIdx[cell], cell))
 
         return connectionHeap
+    
+
+
+
+    
+
+def backtracking2(board):
+    """Takes a board and returns solved board."""
+    # TODO: implement this
+
+    csp = CSP(board)
+
+
+    retval = backtracking_recursive(csp)
+
+    return retval
+
+
+class CSP2(object):
+    def __init__(self, board, _domain=None, _domainLen=None):
+        self.board = board
+        self.domain = {}
+        self.domainLen = {}
+        self.boardIdx = _boardIdx
+        
+        if _domain is None:
+            for row in ROW:
+                for col in COL:
+                    key = f"{row}{col}"
+                    value = self.board[key]
+                    self.domain[key], self.domainLen[key] = (set([value]), 1) if value != 0 else (set(range(1,10)), 9)
+        else:
+            print("Using provided domain, domainLen, assignment")
+            if None in [_domain, _domainLen]:
+                raise ValueError("Either provide all or none of _domain, _domainLen")
+            self.domain    = _domain
+            self.domainLen = _domainLen
+
+    def forwardChecking(self, key):
+        connectionHeap = self.getConnectionsHeap(key)
+        updatedConnections = []
+
+        while connectionHeap:
+            _, _, connectionKey = heapq.heappop(connectionHeap)
+            
+            if self.board[key] in self.domain[connectionKey]:
+                if self.domainLen[connectionKey] == 1:
+                    return False
+                
+                updatedConnections.append(connectionKey)
+
+            else:
+                pass
+
+        for connKey in updatedConnections:
+            self.domainLen[connKey] -= 1
+            self.domain[connKey].remove(self.board[key])
+        return updatedConnections
+
+    def getConnectionsHeap(self, inputCell):
+        connectionHeap = []
+
+        inputRow = inputCell[0]
+        inputCol = inputCell[1]
+
+        # Get Row Connections
+        cols = set(range(1,10))
+        cols.remove(int(inputCol))
+
+        for col in cols:
+            heapq.heappush(connectionHeap, (len(self.domain[f"{inputRow}{col}"]), self.boardIdx[f"{inputRow}{col}"], f"{inputRow}{col}"))
+
+
+        # Get Col Connections
+        rows = set(chr(i) for i in range(65, 74))
+        rows.remove(inputRow)
+
+        for row in rows:
+            heapq.heappush(connectionHeap, (len(self.domain[f"{row}{inputCol}"]), self.boardIdx[f"{row}{inputCol}"], f"{row}{inputCol}"))
+
+        # Get Square Connections
+        cols = []
+        if int(inputCol) <= 3:
+            cols = ['1', '2', '3']
+        elif int(inputCol) >= 7:
+            cols = ['7', '8', '9']
+        else:
+            cols = ['4', '5', '6']
+
+        rows = []
+        if ord(inputRow) - 64 <= 3:
+            rows = ['A', 'B', 'C']
+        elif ord(inputRow) - 64 >= 7:
+            rows = ['G', 'H', 'I']
+        else:
+            rows = ['D', 'E', 'F']
+
+        for row in rows:
+            for col in cols:
+                cell = f"{row}{col}"
+
+                if col == inputCol or row == inputRow:
+                    """
+                    A1 | A2 | A3
+                    -------------
+                    B1 | B2 | B3
+                    -------------
+                    C1 | C2 | C3
+
+                    If Input Cell == B2
+                        -> Row B Added from getRows
+                        -> Col 2 Added from getCols
+
+                    Avoid Duplicates don't add
+                    """
+                    pass
+                else:
+                    heapq.heappush(connectionHeap, (len(self.domain[cell]), self.boardIdx[cell], cell))
+
+        return connectionHeap
+
+
 
 def backtracking(board):
+    csp = CSP2(board)
+    retval = backtrack(csp)
+
+def backtrack(assignment, csp):
+    if checkComplete(assignment):
+        return csp.board
+    else:
+        # Select unassigned variable with MRV
+        var = MRV_select(csp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def backtracking2(board):
     """Takes a board and returns solved board."""
     # TODO: implement this
 
